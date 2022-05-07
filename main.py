b7062f2cc0abd1b3396b51114cf63615c877cc60
Kirlin Mensah <kirlinmensah@gmail.com>
2022-05-06 20:51:15 -0700

    added comments

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def addition(a,b):
-	c = a + b
-	return c
+def addition(a,b):           # this function adds two parameters. 
+	c = a + b            # this stores the result into.
+	return c             # this returns the result when the function is called
 
